ac1de3f9b3f5bc13815abfa059a0931f785f8d53
sappharx <vinnysisk@gmail.com>
2016-01-19 16:15:59 -0700

    another update for coverage

--- coverage.ps1
+++ coverage.ps1
@@ -7,8 +7,8 @@ $filters = "+[Whetstone.*]* -[Whetstone.*.Tests]*"
 $nunitConsole = (Resolve-Path ".\packages\NUnit.Console.*\tools\nunit3-console.exe").ToString()
 $openCover = (Resolve-Path ".\packages\OpenCover.*\tools\OpenCover.Console.exe").ToString()
 $reportGenerator = (Resolve-Path ".\packages\ReportGenerator.*\tools\ReportGenerator.exe").ToString()
-$coveralls = (Resolve-Path ".\packages\coveralls.net.*\tools\csmacnz.Coveralls.exe").ToString()
+#$coveralls = (Resolve-Path ".\packages\coveralls.net.*\tools\csmacnz.Coveralls.exe").ToString()
 
 & $openCover -register:user -target:$nunitConsole -targetargs:$testDLLs "-filter:$filters"
 & $reportGenerator -reports:results.xml -targetdir:coverage
-& $coveralls --opencover -i results.xml --repoToken $repoToken
+#& $coveralls --opencover -i results.xml --repoToken $repoToken
