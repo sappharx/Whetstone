ac4cca39afca20400bb31d36fe8fb159daa4fd38
sappharx <vinnysisk@gmail.com>
2016-01-18 20:36:21 -0700

    create solution and add functional extensions project
    add basic Map<T>() extension method

---
+++ Source/Whetstone.FunctionalExtensions/FunctionalExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    /// <summary>
+    /// Provides some functional capabilities to all types
+    /// </summary>
+    public static class FunctionalExtensions
+    {
+        /// <summary>
+        /// Applies a function on an object of type TSource and returns the resulting object (of type TResult)
+        /// Maps an object of type TSource to an object of type TResult by applying the supplied function to it
+        /// </summary>
+        /// <typeparam name="TSource">The type of the source object</typeparam>
+        /// <typeparam name="TResult">The type of the result object</typeparam>
+        /// <param name="this">The source object</param>
+        /// <param name="func">The function to apply to the source object</param>
+        /// <returns>The result of the provided function applied to the source object</returns>
+        /// <example>
+        /// Parse an integer from a string
+        /// <code>
+        /// int number = "123".Map(int.Parse);  // number == 123
+        /// </code>
+        /// Double a number
+        /// <code>
+        /// int number = 12.Map(x => x * 2);    // number == 24
+        /// </code>
+        /// </example>
+        public static TResult Map<TSource, TResult>(this TSource @this, Func<TSource, TResult> func)
+            => func(@this);
+    }
+}
