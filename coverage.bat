c6ae8c5101403d3c682ea5e492ee336fa743651f
sappharx <vinnysisk@gmail.com>
2016-01-19 12:51:08 -0700

    first attempt at setting up code coverage for Coveralls.io

---
+++ coverage.bat
@@ -0,0 +1,4 @@
+﻿
+.\packages\OpenCover.4.6.166\tools\OpenCover.Console.exe -target:runtests.bat -register:user -filter:"+[Whetstone.*]* -[Whetstone.*.Tests]*"
+
+.\packages\ReportGenerator.2.3.5.0\tools\ReportGenerator.exe -reports:results.xml -targetdir:coverage
