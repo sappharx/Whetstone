b740bc77d7b72d4ae301ff7b0642828addcee26b
sappharx <vinnysisk@gmail.com>
2016-01-19 15:45:20 -0700

    redo coverage script

--- coverage.bat
+++
@@ -1,4 +0,0 @@
-﻿
-.\packages\OpenCover.4.6.166\tools\OpenCover.Console.exe -target:runtests.bat -register:user -filter:"+[Whetstone.*]* -[Whetstone.*.Tests]*"
-
-.\packages\ReportGenerator.2.3.5.0\tools\ReportGenerator.exe -reports:results.xml -targetdir:coverage
---
+++ coverage.ps1
@@ -0,0 +1,14 @@
+﻿$repoToken = "CWDpFsufbHnIxu1NeoLm2rBuOT12lol9U"
+
+$testDLLs = ".\Tests\Whetstone.FunctionalExtensions.Tests\bin\Debug\Whetstone.FunctionalExtensions.Tests.dll"
+$filters = "+[Whetstone.*]* -[Whetstone.*.Tests]*"
+
+# executable file paths
+$nunitConsole = (Resolve-Path ".\packages\NUnit.Console.*\tools\nunit3-console.exe").ToString()
+$openCover = (Resolve-Path ".\packages\OpenCover.*\tools\OpenCover.Console.exe").ToString()
+$reportGenerator = (Resolve-Path ".\packages\ReportGenerator.*\tools\ReportGenerator.exe").ToString()
+$coveralls = (Resolve-Path ".\packages\coveralls.net.*\tools\csmacnz.Coveralls.exe").ToString()
+
+& $openCover -register:user -target:$nunitConsole -targetargs:$testDLLs "-filter:$filters"
+& $reportGenerator -reports:results.xml -targetdir:coverage
+& $coveralls --opencover -i results.xml --repoToken $repoToken
