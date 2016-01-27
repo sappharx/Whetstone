158f98cc14ce99aab7723c331bd105b84aed3f90
sappharx <vinnysisk@gmail.com>
2016-01-26 18:33:07 -0700

    refactor functional extensions tests

--- Tests/Whetstone.FunctionalExtensions.Tests/FunctionalExtensionsTests.cs
+++ Tests/Whetstone.FunctionalExtensions.Tests/FunctionalExtensionsTests.cs
@@ -8,31 +8,55 @@ namespace Whetstone.FunctionalExtensions.Tests
     [TestFixture]
     public class FunctionalExtensionsTests
     {
+        #region Map() tests
+
         [Test]
-        public void MapTest()
+        public void Map_HandlesStaticMethods()
         {
             "123".Map(int.Parse).Should().Be(123, "because Map can handle a static method");
+        }
 
+        [Test]
+        public void Map_HandlesLambdaExpressions()
+        {
             12.Map(x => x * 2).Should().Be(24, "because Map can handle a lambda expression");
+        }
 
+        [Test]
+        public void Map_HandlesIEnumberableInput()
+        {
             new[] { 1, 2, 3 }.Map(arr => new List<int>(arr))
                 .Should().BeOfType<List<int>>("because Map can handle IEnumerable<T> input");
         }
 
+        #endregion
+
+        #region When() tests
+
         [Test]
-        public void WhenTest()
+        public void When_HandlesBooleanParameter()
         {
             // test overload version where predicate is bool
             "abc".When(true, s => s + s).Should().Be("abcabc", "because predicate is true");
             "abc".When(false, s => s + s).Should().Be("abc", "because predicate is false");
+        }
 
+        [Test]
+        public void When_HandlesFuncParameterWithNoParameter()
+        {
             // test overload version where predicate is Func<bool>
             12.When(() => true, x => x * 2).Should().Be(24, "because predicate evaluates to true");
             13.When(() => false, x => x * 2).Should().Be(13, "because predicate evaluates to false");
-            
+        }
+
+        [Test]
+        public void When_HandlesFuncParameterWithParamterOfTypeT()
+        {
             // test overload version where predicate is Func<T, bool>
             12.When(x => x % 2 == 0, x => x / 2).Should().Be(6, "because predicate evaluates to true");
             13.When(x => x % 2 == 0, x => x / 2).Should().Be(13, "because predicate evaluates to false");
         }
+
+        #endregion
     }
 }
