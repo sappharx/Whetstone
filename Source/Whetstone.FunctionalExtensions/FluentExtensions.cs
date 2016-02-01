5ee3d2f9f3470d60906a9195a6297e5f5b23bb6d
sappharx <vinnysisk@gmail.com>
2016-02-01 16:15:07 -0700

    add AsFluentCollection() extension method for easily converting from ICollection<T>

---
+++ Source/Whetstone.FunctionalExtensions/FluentExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Whetstone.FunctionalExtensions
+{
+    /// <summary>
+    /// Provides some fluent APIs to certain types
+    /// </summary>
+    public static class FluentExtensions
+    {
+        /// <summary>
+        /// Creates a new <see cref="FluentCollection{T}"/> from an existing <see cref="ICollection{T}"/> object
+        /// </summary>
+        /// <typeparam name="T">The type of the objects held in original collection</typeparam>
+        /// <param name="this">The original collection object</param>
+        /// <returns>A new <see cref="FluentCollection{T}"/> object wrapping the original collection</returns>
+        public static FluentCollection<T> AsFluentCollection<T>(this ICollection<T> @this)
+            => new FluentCollection<T>(@this);
+    }
+}
---
+++ Tests/Whetstone.FunctionalExtensions.Tests/FluentExtensionsTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Whetstone.FunctionalExtensions.Tests
+{
+    [TestFixture]
+    public class FluentExtensionsTests
+    {
+        [Test]
+        public void AsFluentCollection_WrapsAnICollectionProperly()
+        {
+            new List<int> {1, 2, 3, 4}.AsFluentCollection()
+                .Should().BeOfType<FluentCollection<int>>("because AsFluentCollection wraps the list properly");
+        }
+    }
+}
