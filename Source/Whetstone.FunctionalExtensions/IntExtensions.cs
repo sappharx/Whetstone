7ded1e3c8a38b9b2c9d658cb5f28e5fff3e949f0
sappharx <vinnysisk@gmail.com>
2016-02-01 17:02:40 -0700

    add int extensions

---
+++ Source/Whetstone.FunctionalExtensions/IntExtensions.cs
@@ -0,0 +1,39 @@
+﻿namespace System
+{
+    /// <summary>
+    /// Provides extension methods for integers
+    /// </summary>
+    public static class IntExtensions
+    {
+        /// <summary>
+        /// Returns the value subject to the provided minimum and maximum constraints
+        /// </summary>
+        /// <param name="value">The input value to bound</param>
+        /// <param name="min">The minimum value allowed</param>
+        /// <param name="max">The maximum value allowed</param>
+        /// <returns>The value subjected to the minimum and maximum values allowed</returns>
+        public static int GetBoundedValue(this int value, int min, int max)
+            => Math.Min(Math.Max(value, min), max);
+
+        /// <summary>
+        /// Returns the value (if it's null, use the provided default) subject to the provided minimum constraint
+        /// </summary>
+        /// <param name="value">The input value to bound</param>
+        /// <param name="defaultValue">The value to use if the input value is null</param>
+        /// <param name="min">The minimum value allowed</param>
+        /// <returns>The value (or default) subjected to the minimum values allowed</returns>
+        public static int GetBoundedValue(this int? value, int defaultValue, int min)
+            => Math.Max(value ?? defaultValue, min);
+
+        /// <summary>
+        /// Returns the value (if it's null, use the provided default) subject to the provided minimum and maximum constraints
+        /// </summary>
+        /// <param name="value">The input value to bound</param>
+        /// <param name="defaultValue">The value to use if the input value is null</param>
+        /// <param name="min">The minimum value allowed</param>
+        /// <param name="max">The maximum value allowed</param>
+        /// <returns>The value (or default) subjected to the minimum and maximum values allowed</returns>
+        public static int GetBoundedValue(this int? value, int defaultValue, int min, int max)
+            => GetBoundedValue(value ?? defaultValue, min, max);
+    }
+}
---
+++ Tests/Whetstone.FunctionalExtensions.Tests/IntExtensionsTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Whetstone.FunctionalExtensions.Tests
+{
+    [TestFixture]
+    public class IntExtensionsTests
+    {
+        [TestCaseSource(typeof(NonNullInputWithMinAndMaxCases))]
+        public void GetBoundedValue_HandlesNonNullInputValue(int val, int min, int max, int answer)
+        {
+            val.GetBoundedValue(min, max)
+                .Should().Be(answer, "because GetBoundedValue hanldes non-null input values");
+        }
+
+        [TestCaseSource(typeof(NullableInputWithMinCases))]
+        public void GetBoundedValue_HandlesNullableInputWithMin(int? val, int def, int min, int answer)
+        {
+            val.GetBoundedValue(def, min)
+                .Should().Be(answer, "because GetBoundedValue handles nullable input values with just minimum");
+        }
+
+        [TestCaseSource((typeof (NullableInputWithMinAndMaxCases)))]
+        public void GetBoundedValue_HandlesNullableInputWithMinAndMax(int? val, int def, int min, int max, int answer)
+        {
+            val.GetBoundedValue(def, min, max)
+                .Should().Be(answer, "because GetBoundedValues handles nullable input values with minimum and maximum");
+        }
+    }
+
+    internal class NonNullInputWithMinAndMaxCases : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[] {-1, 0, 2, 0};
+            yield return new object[] { 1, 0, 2, 1 };
+            yield return new object[] { 3, 0, 2, 2 };
+        }
+    }
+
+    internal class NullableInputWithMinCases : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[] { null, -1, 0, 0 };
+            yield return new object[] { null, 1, 0, 1 };
+            yield return new object[] { -1, 0, 0, 0 };
+            yield return new object[] { 1, 0, 0, 1 };
+        }
+    }
+
+    internal class NullableInputWithMinAndMaxCases : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[] { null, -1, 0, 2, 0 };
+            yield return new object[] { null, 1, 0, 2, 1 };
+            yield return new object[] { null, 3, 0, 2, 2 };
+            yield return new object[] { -1, 0, 0, 2, 0 };
+            yield return new object[] { 1, 0, 0, 2, 1 };
+            yield return new object[] { 3, 0, 0, 2, 2 };
+        }
+    }
+}
