f7ebcfd8bf678d6dc4c03239bffd2354c2c7cade
sappharx <vinnysisk@gmail.com>
2016-02-05 15:45:29 -0700

    add Disposable.Using

---
+++ Source/Whetstone.FunctionalExtensions/Disposable.cs
@@ -0,0 +1,42 @@
+﻿namespace System
+{
+    /// <summary>
+    /// Provides static methods for working with <see cref="IDisposable"/> objects
+    /// </summary>
+    public static class Disposable
+    {
+        /// <summary>
+        /// Abstracts a using statement when you just need to obtain a value from an <see cref="IDisposable"/> object
+        /// </summary>
+        /// <typeparam name="TDisposable">The type of the <see cref="IDisposable"/> object</typeparam>
+        /// <typeparam name="TResult">The type of the object to return</typeparam>
+        /// <param name="factory">The function that creates the disposable object</param>
+        /// <param name="func">The function to apply to the disposable object to obtain the result</param>
+        /// <returns>
+        /// The value obtained by applying the supplied function to the created disposable object
+        /// </returns>
+        /// <example>
+        /// Create a string array by reading a file and splitting it into lines
+        /// <code>
+        /// // `GetFileStream` is a method that returns a StreamReader for the specific file, i.e. it takes no arguments
+        /// var lines = Disposable.Using(GetFileStream, reader => reader.ReadToEnd().Split(System.Environment.Newline));
+        /// </code>
+        /// as opposed to
+        /// <code>
+        /// string[] lines;
+        /// using (var reader = GetFileStream())
+        /// {
+        ///     lines = reader.ReadToEnd().Split(System.Environment.Newline);
+        /// }
+        /// </code>
+        /// </example>
+        public static TResult Using<TDisposable, TResult>(Func<TDisposable> factory, Func<TDisposable, TResult> func)
+            where TDisposable : IDisposable
+        {
+            using (var disposable = factory())
+            {
+                return func(disposable);
+            }
+        }
+    }
+}
---
+++ Tests/Whetstone.FunctionalExtensions.Tests/DisposableTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Whetstone.FunctionalExtensions.Tests
+{
+    [TestFixture]
+    public class DisposableTests
+    {
+        [Test]
+        public void Using_WorksProperly()
+        {
+            CreateTestFile();
+
+            Disposable.Using(GetReader, reader => reader.ReadToEnd())
+                .Should().Be("foo", "because Using() works properly");
+        }
+
+        private static StreamReader GetReader()
+            => new StreamReader("test.txt");
+
+        private static void CreateTestFile()
+        {
+            using (var writer = new StreamWriter("test.txt"))
+            {
+                writer.Write("foo");
+            }
+        }
+    }
+}
