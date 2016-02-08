3e09e8fb1dfe681128c181562c654f541afdc207
sappharx <vinnysisk@gmail.com>
2016-02-08 11:51:29 -0700

    rename project to just Whetstone

--- Source/Whetstone.FunctionalExtensions/Properties/AssemblyInfo.cs
+++ Source/Whetstone.FunctionalExtensions/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Whetstone.FunctionalExtensions")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Whetstone")]
+[assembly: AssemblyDescription("Toolbox of functional extension methods and other useful tools for sharpening your .NET code")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Whetstone.FunctionalExtensions")]
+[assembly: AssemblyCompany("Vincent Sisk")]
+[assembly: AssemblyProduct("Whetstone")]
 [assembly: AssemblyCopyright("Copyright © Vincent Sisk 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.1")]
-[assembly: AssemblyFileVersion("0.0.0.1")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
