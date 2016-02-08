624ac3f20bbf00adeaf83823daacff5eec396944
sappharx <vinnysisk@gmail.com>
2016-02-08 16:27:25 -0700

    Revert "revert coverage script"
    This reverts commit e6a51a13734a4892b03b7467164f40f0075936f6.

--- Scripts/coverage.ps1
+++ Scripts/coverage.ps1
@@ -1,13 +1,13 @@
 ﻿$repoToken = "CWDpFsufbHnIxu1NeoLm2rBuOT12lol9U"
 
-$testDLLs = ".\Tests\Whetstone.Tests\bin\$env:CONFIGURATION\Whetstone.Tests.dll"
+$testDLLs = "..\Tests\Whetstone.Tests\bin\$env:CONFIGURATION\Whetstone.Tests.dll"
 $filters = "+[Whetstone*]* -[Whetstone*.Tests]*"
 
 # executable file paths
-$nunitConsole = (Resolve-Path ".\packages\NUnit.Console.*\tools\nunit3-console.exe").ToString()
-$openCover = (Resolve-Path ".\packages\OpenCover.*\tools\OpenCover.Console.exe").ToString()
-$reportGenerator = (Resolve-Path ".\packages\ReportGenerator.*\tools\ReportGenerator.exe").ToString()
-$coveralls = (Resolve-Path ".\packages\coveralls.net.*\tools\csmacnz.Coveralls.exe").ToString()
+$nunitConsole = (Resolve-Path "..\packages\NUnit.Console.*\tools\nunit3-console.exe").ToString()
+$openCover = (Resolve-Path "..\packages\OpenCover.*\tools\OpenCover.Console.exe").ToString()
+$reportGenerator = (Resolve-Path "..\packages\ReportGenerator.*\tools\ReportGenerator.exe").ToString()
+$coveralls = (Resolve-Path "..\packages\coveralls.net.*\tools\csmacnz.Coveralls.exe").ToString()
 
 & $openCover -register:user -target:$nunitConsole -targetargs:$testDLLs "-filter:$filters"
 & $reportGenerator -reports:results.xml -targetdir:coverage
